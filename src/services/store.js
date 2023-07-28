60e141d10ee46637668a13e63feb3a696c6463af
Henry Houle <henryhoule17@gmail.com>
2023-07-27 21:37:23 -0700

    cleared cache

--- src/services/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { topicApi } from "./api"
-
-export const store = configureStore({
-    reducer: {
-        [topicApi.reducerPath]: topicApi.reducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(topicApi.middleware)
-});
